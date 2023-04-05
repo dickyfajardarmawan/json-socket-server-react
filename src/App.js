7efc4506bb0b521e7e2d372a65d290cedca89346
Dicky <dickyfajardarmawan@gmail.com>
2023-04-05 15:42:44 +0700

    adfklj

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WebSoket from './WebSoket';
+
+function App() {
+  return (
+    <div className="App">
+      <WebSoket />
+    </div>
+  );
+}
+
+export default App;
